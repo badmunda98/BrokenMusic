5a89bc5cffd21bf6584a4becb956bcfc602f12ab
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-02-03 23:28:48 +0530

    Update thumbnails.py

--- VIPMUSIC/utils/thumbnails.py
+++ VIPMUSIC/utils/thumbnails.py
@@ -103,7 +103,7 @@ async def get_thumb(videoid):
         para = textwrap.wrap(clear(title), width=32) 
         j = 0
         draw.text(
-            (6, 6), f"{BOT_NAME}", fill="blue", font=name_font
+            (6, 6), f"{BOT_NAM}", fill="blue", font=name_font
         )
         draw.text(
             (600, 200),
